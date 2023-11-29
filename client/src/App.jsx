d81c4a582660a3d11ba373da181f4e15d081e20a
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2023-11-29 03:29:17 +0200

    Add react rout library
    Add game list page
    Add navigation links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,8 @@
+import {Routes, Route} from 'react-router-dom'
+
 import Header from "./components/header/Header"
+import Home from "./components/home/Home"
+import GameList from './components/game-list/Game-list'
 
 
 function App() {
@@ -6,6 +10,11 @@ function App() {
   return (
     <div id="box">
      <Header />
+     <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/games' element={<GameList />} />
+     </Routes>
+     <Home />
       </div>
      
     
