ae20775e72df27b52eed53e83cf30d284d609812
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2023-12-02 01:23:19 +0200

    Change Login to controlled.json

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,16 +1,34 @@
+import useForm from "../../hooks/useForms";
+
 export default function Login(){
+    const {values, onChange, onSubmit} = useForm({
+        email:'',
+    password:''
+});
     return(
         <section id="login-page" className="auth">
-        <form id="login">
+        <form id="login" onSubmit={onSubmit}>
 
             <div className="container">
                 <div className="brand-logo"></div>
                 <h1>Login</h1>
                 <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
+                <input 
+                type="email" 
+                id="email" 
+                name="email" 
+                placeholder="Sokka@gmail.com"
+                value={values.email}
+                onChange={onChange} />
 
                 <label htmlFor="login-pass">Password:</label>
-                <input type="password" id="login-password" name="password" />
+                <input 
+                type="password" 
+                id="login-password" 
+                name="password" 
+                value={values.password}
+                onChange={onChange}/>
+                
                 <input type="submit" className="btn submit" value="Login" />
                 <p className="field">
                     <span>If you don't have profile click <a href="#">here</a></span>
